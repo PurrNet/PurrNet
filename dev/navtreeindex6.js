759d37b410ba1bbc77c6fa4de8a334e9bf375d3a
BobsiUnity <BobsiUnity@users.noreply.github.com>
2025-12-01 20:52:10 +0000

    deploy: 7f8a1bea01ad00d634ac2d7a12170cd41cd3d5f5

--- dev/navtreeindex6.js
+++ dev/navtreeindex6.js
@@ -1,8 +1,10 @@
 var NAVTREEINDEX6 =
 {
+"structPurrNet_1_1SyncStatus.html":[1,0,0,130],
+"structPurrNet_1_1Transports_1_1ByteData.html":[1,0,0,9,1],
 "structPurrNet_1_1Transports_1_1ByteData.html":[0,0,0,9,1],
-"structPurrNet_1_1Transports_1_1ClientJoinInfo.html":[0,0,0,9,2],
 "structPurrNet_1_1Transports_1_1ClientJoinInfo.html":[1,0,0,9,2],
+"structPurrNet_1_1Transports_1_1ClientJoinInfo.html":[0,0,0,9,2],
 "structPurrNet_1_1Transports_1_1Connection.html":[0,0,0,9,4],
 "structPurrNet_1_1Transports_1_1Connection.html":[1,0,0,9,4],
 "structPurrNet_1_1Transports_1_1HostJoinInfo.html":[0,0,0,9,6],
