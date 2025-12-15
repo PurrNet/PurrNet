69f50a2531c5bd4e7da2ea6a75e0e00477a4fb87
BlenMiner <blenminer123@gmail.com>
2025-12-15 09:42:30 +0100

    some tests

--- Assets/Examples/Top Down Shooter/Scripts/PlayerHeadLamp.cs
+++ Assets/Examples/Top Down Shooter/Scripts/PlayerHeadLamp.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 
 public class PlayerHeadLamp : NetworkIdentity
 {
+    private readonly SyncList<int> _playerStats = new SyncList<int>();
     public SyncVar<bool> _lampIsOn = new SyncVar<bool>(true, ownerAuth: true);
+
+    protected override void OnSpawned(bool asServer)
+    {
+        if (asServer)
+        {
+            Debug.Log("I am the server, adding player stats");
+            _playerStats.Clear();
+            _playerStats.Add(1);
+        }
+    }
 
     protected override void OnSpawned()
     {
