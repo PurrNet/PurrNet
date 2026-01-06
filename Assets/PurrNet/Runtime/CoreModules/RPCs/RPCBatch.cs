5d1d6f9e0b48f5e30576bca8d90f2e63285d256f
BlenMiner <blenminer123@gmail.com>
2026-01-06 10:19:00 +0100

    remove similar batching

--- Assets/PurrNet/Runtime/CoreModules/RPCs/RPCBatch.cs
+++ Assets/PurrNet/Runtime/CoreModules/RPCs/RPCBatch.cs
@@ -15,10 +15,7 @@ namespace PurrNet.Modules
         static readonly ProfilerMarker _flushMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.Flush");
         static readonly ProfilerMarker _flushChannelMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.FlushChannel");
         static readonly ProfilerMarker _getSingleBatchMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.GetBatchIndex");
-        static readonly ProfilerMarker _queueMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.Queue");
-        static readonly ProfilerMarker _queueSimilarSearchMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.Queue(Gathering Similar Batches)");
-        static readonly ProfilerMarker _queueSimilarSendMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.Queue(Sending Similar Batches)");
-        static readonly ProfilerMarker _queueSingleMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.Queue(Single)");
+        static readonly ProfilerMarker _queueSingleMarker = new ProfilerMarker($"RPCBatch<{typeof(HEADER).Name}>.Queue");
 
         struct PendingBatchedData
         {
@@ -34,28 +31,6 @@ namespace PurrNet.Modules
         {
             public Size count;
             public BitPacker data;
-        }
-
-        struct UsersBatchKey : IEquatable<UsersBatchKey>
-        {
-            public HEADER lastHeader;
-            public Size lastDataLen;
-
-            public bool Equals(UsersBatchKey other)
-            {
-                return lastHeader.Equals(other.lastHeader) &&
-                       lastDataLen.Equals(other.lastDataLen);
-            }
-
-            public override bool Equals(object obj)
-            {
-                return obj is UsersBatchKey other && Equals(other);
-            }
-
-            public override int GetHashCode()
-            {
-                return HashCode.Combine(lastHeader, lastDataLen);
-            }
         }
 
         private readonly PlayersManager _playersManager;
@@ -103,7 +78,7 @@ namespace PurrNet.Modules
         {
             using (_flushChannelMarker.Auto())
             {
-                bool sentOne = false;
+                bool removed = false;
                 for (int i = _batches.Count - 1; i >= 0; i--)
                 {
                     var batch = _batches[i];
@@ -112,13 +87,17 @@ namespace PurrNet.Modules
                         continue;
 
                     SendBatch(batch);
-                    sentOne = true;
                     batch.batchedData.Dispose();
-                    _batches.RemoveAt(i--);
+                    _batches.RemoveAt(i);
+                    removed = true;
                 }
 
-                if (sentOne)
+                if (removed)
+                {
                     _batchIndexMap.Clear();
+                    for (int i = 0; i < _batches.Count; i++)
+                        _batchIndexMap[_batches[i].key] = i;
+                }
             }
         }
 
@@ -137,20 +116,10 @@ namespace PurrNet.Modules
         {
             using (_getSingleBatchMarker.Auto())
             {
-                int c = _batches.Count;
-
                 if (_batchIndexMap.TryGetValue(key, out int idx))
                     return idx;
 
-                for (var i = c - 1; i >= 0; i--)
-                {
-                    if (BatchKey.AreEquals(key, _batches[i].key))
-                    {
-                        _batchIndexMap[key] = i;
-                        return i;
-                    }
-                }
-
+                int c = _batches.Count;
                 _batches.Add(new PendingBatchedData { key = key, batchedData = BitPackerPool.Get() });
                 _batchIndexMap[key] = c;
                 return c;
@@ -182,67 +151,11 @@ namespace PurrNet.Modules
 
         public void Queue(DisposableList<PlayerID> targets, HEADER header, ByteData content, Channel channel)
         {
-            using (_queueMarker.Auto())
-            {
-                Size len = content.length;
-                var similarBatches = DisposableDictionary<UsersBatchKey, DisposableList<PlayerID>>.Create();
-                similarBatches.dictionary.EnsureCapacity(targets.Count);
-
-                using (_queueSimilarSearchMarker.Auto())
-                {
-                    for (var i = targets.Count - 1; i >= 0; i--)
-                    {
-                        var target = targets[i];
-
-                        UsersBatchKey key;
-
-                        if (_batchIndexMap.TryGetValue(new BatchKey { playerId = target, channel = channel },
-                                out int idx))
-                        {
-                            var batch = _batches[idx];
-                            key = new UsersBatchKey { lastHeader = batch.lastHeader, lastDataLen = batch.lastDataLen };
-                        }
-                        else key = new UsersBatchKey { lastHeader = default, lastDataLen = default };
-
-                        if (similarBatches.TryGetValue(key, out var list))
-                        {
-                            list.Add(target);
-                        }
-                        else
-                        {
-                            var newList = DisposableList<PlayerID>.Create();
-                            newList.Add(target);
-                            similarBatches[key] = newList;
-                        }
-                    }
-                }
-
-                using (_queueSimilarSendMarker.Auto())
-                {
-                    using (var enumerator = similarBatches.dictionary.GetEnumerator())
-                    {
-                        while (enumerator.MoveNext())
-                        {
-                            var key = enumerator.Current.Key;
-                            var value = enumerator.Current.Value;
-
-                            using var packer = BitPackerPool.Get();
-                            DeltaPacker<HEADER>.WriteFunc(packer, key.lastHeader, header);
-                            DeltaPackInteger.WriteIndex(packer, key.lastDataLen, len);
-
-                            for (var i = value.Count - 1; i >= 0; i--)
-                                Queue(value[i], packer, header, content, channel);
-
-                            value.Dispose();
-                        }
-                    }
-                }
-
-                similarBatches.Dispose();
-            }
+            for (var i = targets.Count - 1; i >= 0; i--)
+                Queue(targets[i], header, content, channel);
         }
 
-        public void Queue(PlayerID target, BitPacker header, HEADER headerVal, ByteData content, Channel channel)
+        public void Queue(PlayerID target, BitPacker header, BitPacker defaultHeader, HEADER headerVal, ByteData content, Channel channel)
         {
             using (_queueSingleMarker.Auto())
             {
@@ -258,11 +171,9 @@ namespace PurrNet.Modules
                     {
                         SendBatch(batch);
                         batch.batchCount = 0;
-                        batch.lastHeader = default;
-                        batch.lastDataLen = default;
+                        batch.cachedMTU = null;
                         batch.batchedData.ResetPositionAndMode(false);
-                        DeltaPacker<HEADER>.WriteFunc(batch.batchedData, batch.lastHeader, headerVal);
-                        DeltaPackInteger.WriteIndex(batch.batchedData, batch.lastDataLen, content.length);
+                        batch.batchedData.WriteBits(defaultHeader);
                     }
                     else
                     {
