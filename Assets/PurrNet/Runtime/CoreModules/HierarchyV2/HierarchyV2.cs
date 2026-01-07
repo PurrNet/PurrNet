4abb4ab997672110c0f100be5cecd2db4b52087f
BlenMiner <blenminer123@gmail.com>
2026-01-07 22:06:33 +0100

    complete pending spawns before on despawn

--- Assets/PurrNet/Runtime/CoreModules/HierarchyV2/HierarchyV2.cs
+++ Assets/PurrNet/Runtime/CoreModules/HierarchyV2/HierarchyV2.cs
@@ -1265,6 +1265,10 @@ namespace PurrNet.Modules
                 return;
             }
 
+            // Try to despawn the object properly if despawn was on the same tick (by first calling OnSpawned)
+            for (var i = 0; i < c; i++)
+                CompletePendingSpawnsFor(children[i]);
+
             if (_asServer)
             {
                 _visibility.ClearVisibilityForGameObject(gameObject.transform);
@@ -1419,6 +1423,27 @@ namespace PurrNet.Modules
             FlushSpawnPackets();
             _manager.FlushBatchedRPCs();
             SpawnDelayedIdentities();
+        }
+
+        private void CompletePendingSpawnsFor(NetworkIdentity toSpawn)
+        {
+            bool isHost = IsServerHost();
+
+            if (_toSpawnNextFrame.Remove(toSpawn))
+            {
+                if (!toSpawn || !toSpawn.isSpawned)
+                    return;
+
+                toSpawn.TriggerSpawnEvent(_asServer);
+
+                if (_asServer && isHost)
+                {
+                    toSpawn.SetIsSpawned(true, false);
+                    toSpawn.TriggerSpawnEvent(false);
+                }
+
+                onIdentityAdded?.Invoke(toSpawn);
+            }
         }
 
         private void SendDelayedObserverEvents()
