668e8d611cbdda087022cd6cd054748146efde38
BlenMiner <blenminer123@gmail.com>
2026-01-07 15:35:58 +0100

    remove no longer needed code

--- Assets/Benchmarks/PositionWithSyncVar/PositionWithSyncVar.cs
+++ Assets/Benchmarks/PositionWithSyncVar/PositionWithSyncVar.cs
@@ -1,6 +1,4 @@
 using PurrNet;
-using PurrNet.Packing;
-using PurrNet.Transports;
 using UnityEngine;
 
 public class PositionWithSyncVar : NetworkIdentity
@@ -10,28 +8,8 @@ public class PositionWithSyncVar : NetworkIdentity
     float timer = 3;
     Vector3 moveDir = Vector3.zero;
 
-    [TargetRpc(channel: Channel.Unreliable, runLocally: true)]
-    void Test(PlayerID player, PackedUInt integer, BitPacker v, BitPacker a)
-    {
-        using (v)
-        {
-            string result = Packer<string>.Read(v);
-            string resultb = Packer<string>.Read(a);
-            Debug.Log(integer);
-            Debug.Log(result);
-            Debug.Log(resultb);
-        }
-    }
-
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            using var packer = BitPackerPool.Get();
-            Packer<string>.Write(packer, "YO YO YO 69 70?");
-            Test(new PlayerID(1, false), (uint)packer.positionInBits, packer, packer);
-        }
-
         if (isOwner)
         {
             transform.position += moveDir * Time.deltaTime;
