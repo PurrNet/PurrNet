890a29cd72bf96a70665da77c37aca34caaaa187
BlenMiner <blenminer123@gmail.com>
2026-01-27 09:26:48 +0100

    fix: using after free

--- Assets/PurrNet/Runtime/Components/NetworkModule/SyncEvent.cs
+++ Assets/PurrNet/Runtime/Components/NetworkModule/SyncEvent.cs
@@ -14,8 +14,7 @@ namespace PurrNet
 
         public SyncEventData AddData<T>(T data)
         {
-            if(_dataPacker == null)
-                _dataPacker = BitPackerPool.Get();
+            _dataPacker ??= BitPackerPool.Get();
             Packer<T>.Write(_dataPacker, data);
             return this;
         }
@@ -24,7 +23,7 @@ namespace PurrNet
         {
             if (_dataPacker == null)
                 return default;
-            
+
             return Packer<T>.Read(_dataPacker);
         }
 
@@ -35,10 +34,14 @@ namespace PurrNet
 
         public void Dispose()
         {
-            _dataPacker.Dispose();
+            if (_dataPacker != null)
+            {
+                _dataPacker.Dispose();
+                _dataPacker = null;
+            }
         }
     }
-    
+
     [Serializable]
     public abstract class SyncEventBase : NetworkModule
     {
@@ -72,7 +75,7 @@ namespace PurrNet
 
         protected abstract void InvokeLocal();
     }
-    
+
     [Serializable]
     public abstract class SyncEventLogic<T> : SyncEventBase
     {
@@ -94,7 +97,7 @@ namespace PurrNet
                 if (isServer) SendToAll(data);
                 else SendToServer(data);
             }
-            
+
             _lastData.ResetPosition();
             InvokeLocal();
         }
@@ -115,34 +118,37 @@ namespace PurrNet
         [ObserversRpc(Channel.ReliableOrdered, excludeOwner: true)]
         private void SendToOthers(SyncEventData data)
         {
-            using (data)
+            if (isServer && !isHost)
             {
-                if (isServer && !isHost) return;
-                _lastData = data;
-                InvokeLocal();
+                data.Dispose();
+                return;
             }
+
+            _lastData.Dispose();
+            _lastData = data;
+
+            InvokeLocal();
         }
 
         [ObserversRpc(Channel.ReliableOrdered)]
         private void SendToAll(SyncEventData data)
         {
-            using (data)
+            if (!isHost)
             {
-                if (!isHost)
-                {
-                    _lastData = data;
-                    InvokeLocal();
-                }
+                _lastData.Dispose();
+                _lastData = data;
+                InvokeLocal();
             }
+            else data.Dispose();
         }
 
         [ObserversRpc(runLocally: true)]
         private void RemoveAllListenersRpc() => ClearUnityEvent();
     }
-    
+
     [Serializable]
     public class SerializableSyncUnityEvent : UnityEvent { }
-    
+
     [Serializable]
     public class SyncEvent : SyncEventLogic<byte>
     {
@@ -162,7 +168,7 @@ namespace PurrNet
 
         protected override void InvokeUnityEvent(byte data) => unityEvent?.Invoke();
         protected override void ClearUnityEvent() => unityEvent.RemoveAllListeners();
-        
+
         public static SyncEvent operator +(SyncEvent e, UnityAction listener)
         {
             if (e == null) throw new ArgumentNullException(nameof(e));
@@ -177,10 +183,10 @@ namespace PurrNet
             return e;
         }
     }
-    
+
     [Serializable]
     public class SerializableSyncUnityEvent<T> : UnityEvent<T> { }
-    
+
     [Serializable]
     public class SyncEvent<T> : SyncEventLogic<T>
     {
@@ -201,7 +207,7 @@ namespace PurrNet
 
         protected override void InvokeUnityEvent(T arg) => unityEvent?.Invoke(arg);
         protected override void ClearUnityEvent() => unityEvent.RemoveAllListeners();
-        
+
         public static SyncEvent<T> operator +(SyncEvent<T> e, UnityAction<T> listener)
         {
             if (e == null) throw new ArgumentNullException(nameof(e));
@@ -231,17 +237,17 @@ namespace PurrNet
         public void RemoveListener(UnityAction<T1, T2> listener) => unityEvent.RemoveListener(listener);
 
         public void Invoke(T1 arg1, T2 arg2) => InvokePacket(new SyncEventData().AddData<T1>(arg1).AddData<T2>(arg2));
-        
+
         protected override void InvokeLocal()
         {
             T1 value1 = _lastData.ReadData<T1>();
             T2 value2 = _lastData.ReadData<T2>();
             unityEvent.Invoke(value1, value2);
         }
-        
+
         protected override void InvokeUnityEvent((T1, T2) data) => unityEvent?.Invoke(data.Item1, data.Item2);
         protected override void ClearUnityEvent() => unityEvent.RemoveAllListeners();
-        
+
         public static SyncEvent<T1, T2> operator +(SyncEvent<T1, T2> e, UnityAction<T1, T2> listener)
         {
             if (e == null) throw new ArgumentNullException(nameof(e));
@@ -256,17 +262,17 @@ namespace PurrNet
             return e;
         }
     }
-    
+
     [Serializable]
     public class SerializableSyncUnityEvent<T1, T2, T3> : UnityEvent<T1, T2, T3> { }
-    
+
     [Serializable]
     public class SyncEvent<T1, T2, T3> : SyncEventLogic<(T1, T2, T3)>
     {
         [SerializeField] private SerializableSyncUnityEvent<T1, T2, T3> unityEvent = new SerializableSyncUnityEvent<T1, T2, T3>();
 
         public SyncEvent(bool ownerAuth = false) : base(ownerAuth) { }
-    
+
         public void AddListener(UnityAction<T1, T2, T3> listener) => unityEvent.AddListener(listener);
         public void RemoveListener(UnityAction<T1, T2, T3> listener) => unityEvent.RemoveListener(listener);
 
@@ -278,10 +284,10 @@ namespace PurrNet
             T3 value3 = _lastData.ReadData<T3>();
             unityEvent.Invoke(value1, value2, value3);
         }
-        
+
         protected override void InvokeUnityEvent((T1, T2, T3) data) => unityEvent?.Invoke(data.Item1, data.Item2, data.Item3);
         protected override void ClearUnityEvent() => unityEvent.RemoveAllListeners();
-        
+
         public static SyncEvent<T1, T2, T3> operator +(SyncEvent<T1, T2, T3> e, UnityAction<T1, T2, T3> listener)
         {
             if (e == null) throw new ArgumentNullException(nameof(e));
@@ -296,17 +302,17 @@ namespace PurrNet
             return e;
         }
     }
-    
+
     [Serializable]
     public class SerializableSyncUnityEvent<T1, T2, T3, T4> : UnityEvent<T1, T2, T3, T4> { }
-    
+
     [Serializable]
     public class SyncEvent<T1, T2, T3, T4> : SyncEventLogic<(T1, T2, T3, T4)>
     {
         [SerializeField] private SerializableSyncUnityEvent<T1, T2, T3, T4> unityEvent = new SerializableSyncUnityEvent<T1, T2, T3, T4>();
 
         public SyncEvent(bool ownerAuth = false) : base(ownerAuth) { }
-    
+
         public void AddListener(UnityAction<T1, T2, T3, T4> listener) => unityEvent.AddListener(listener);
         public void RemoveListener(UnityAction<T1, T2, T3, T4> listener) => unityEvent.RemoveListener(listener);
 
@@ -319,10 +325,10 @@ namespace PurrNet
             T4 value4 = _lastData.ReadData<T4>();
             unityEvent.Invoke(value1, value2, value3, value4);
         }
-        
+
         protected override void InvokeUnityEvent((T1, T2, T3, T4) data) => unityEvent?.Invoke(data.Item1, data.Item2, data.Item3, data.Item4);
         protected override void ClearUnityEvent() => unityEvent.RemoveAllListeners();
-        
+
         public static SyncEvent<T1, T2, T3, T4> operator +(SyncEvent<T1, T2, T3, T4> e, UnityAction<T1, T2, T3, T4> listener)
         {
             if (e == null) throw new ArgumentNullException(nameof(e));
@@ -337,14 +343,14 @@ namespace PurrNet
             return e;
         }
     }
-    
+
     [Serializable]
     public class SyncEvent<T1, T2, T3, T4, T5> : SyncEventLogic<(T1, T2, T3, T4, T5)>
     {
         private event Action<T1, T2, T3, T4, T5> unityEvent;
-        
+
         public SyncEvent(bool ownerAuth = false) : base(ownerAuth) { }
-    
+
         public void AddListener(Action<T1, T2, T3, T4, T5> listener) => unityEvent += listener;
         public void RemoveListener(Action<T1, T2, T3, T4, T5> listener) => unityEvent -= listener;
 
@@ -358,9 +364,9 @@ namespace PurrNet
             T5 value5 = _lastData.ReadData<T5>();
             unityEvent.Invoke(value1, value2, value3, value4, value5);
         }
-        
+
         protected override void InvokeUnityEvent((T1, T2, T3, T4, T5) data) => unityEvent?.Invoke(data.Item1, data.Item2, data.Item3, data.Item4, data.Item5);
-        
+
         public static SyncEvent<T1, T2, T3, T4, T5> operator +(SyncEvent<T1, T2, T3, T4, T5> e, Action<T1, T2, T3, T4, T5> listener)
         {
             if (e == null) throw new ArgumentNullException(nameof(e));
