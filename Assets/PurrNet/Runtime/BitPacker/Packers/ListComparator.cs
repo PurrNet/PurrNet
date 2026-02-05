4c78de3b670b700bd731d1a5020ab22945ec69f2
BlenMiner <blenminer123@gmail.com>
2026-02-06 00:09:12 +0100

    refactor ListComparator to no cache element equality comparer

--- Assets/PurrNet/Runtime/BitPacker/Packers/ListComparator.cs
+++ Assets/PurrNet/Runtime/BitPacker/Packers/ListComparator.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace PurrNet.Packing
 {
     internal readonly struct ListComparator<T> : IEqualityComparer<List<T>>
     {
-        static readonly IEqualityComparer<T> eq = PurrEquality<T>.Default;
-
         public bool Equals(List<T> x, List<T> y)
         {
             if (ReferenceEquals(x, y)) return true;
@@ -14,10 +12,11 @@ namespace PurrNet.Packing
             if (x.Count != y.Count) return false;
 
             int count = x.Count;
+            var elementEquality = PurrEquality<T>.Default;
 
             for (int i = 0; i < count; i++)
             {
-                if (!eq.Equals(x[i], y[i]))
+                if (!elementEquality.Equals(x[i], y[i]))
                     return false;
             }
             return true;
