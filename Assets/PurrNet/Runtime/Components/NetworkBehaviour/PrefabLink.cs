e09adee2e93b44f696c3836bbefe7f31b4e78c8f
BlenMiner <blenminer123@gmail.com>
2026-02-07 23:32:17 +0100

    obsolete and custom inspector were clashing

--- Assets/PurrNet/Runtime/Components/NetworkBehaviour/PrefabLink.cs
+++ Assets/PurrNet/Runtime/Components/NetworkBehaviour/PrefabLink.cs
@@ -1,10 +1,8 @@
-using System;
 using UnityEngine;
 
 namespace PurrNet
 {
     [AddComponentMenu("")]
-    [Obsolete("PrefabLink is no longer used by PurrNet and can be safely removed.")]
     public sealed class PrefabLink : NetworkIdentity
     {
     }
