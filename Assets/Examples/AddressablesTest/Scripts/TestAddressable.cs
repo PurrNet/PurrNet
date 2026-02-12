4d44dcc08f1a2bdfacb8b46ca9058318ab8911b8
BobsiUnity <martin.skovrup@gmail.com>
2026-02-13 00:31:55 +0100

    Actually call the damn RPC...

--- Assets/Examples/AddressablesTest/Scripts/TestAddressable.cs
+++ Assets/Examples/AddressablesTest/Scripts/TestAddressable.cs
@@ -9,9 +9,10 @@ namespace AddressablesTest
         {
             base.OnSpawned();
             if (isController)
+            {
                 transform.position += new Vector3(Random.Range(-5f, 5f), Random.Range(0f, 2f), Random.Range(-5f, 5f));
-            
-            
+                TestRpc();
+            }
         }
 
         [ObserversRpc]
