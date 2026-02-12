331707f42aa7863c2ebb76570131a0217759822e
BobsiUnity <martin.skovrup@gmail.com>
2026-02-13 00:20:34 +0100

    More testing

---
+++ Assets/Examples/AddressablesTest/Scripts/TestAddressable.cs
@@ -0,0 +1,29 @@
+using PurrNet;
+using UnityEngine;
+
+namespace AddressablesTest
+{
+    public class TestAddressable : NetworkIdentity
+    {
+        protected override void OnSpawned()
+        {
+            base.OnSpawned();
+            if (isController)
+                transform.position += new Vector3(Random.Range(-5f, 5f), Random.Range(0f, 2f), Random.Range(-5f, 5f));
+            
+            
+        }
+
+        [ObserversRpc]
+        private void TestRpc()
+        {
+            Debug.Log($"Yo dawg, I was just spawned!");
+        }
+
+        [ObserversRpc, PurrButton]
+        private void AnotherTestRpc()
+        {
+            Debug.Log($"WITNESS ME!");
+        }
+    }
+}
