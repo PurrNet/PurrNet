e856044a51482ef2c4c745a4280a6558eeec2453
BlenMiner <blenminer123@gmail.com>
2026-02-25 12:47:44 +0100

    refactor: simplify ClearTrackers method by removing unnecessary key lookups

--- Assets/PurrNet/Runtime/CoreModules/DeltaModule/DeltaModule.cs
+++ Assets/PurrNet/Runtime/CoreModules/DeltaModule/DeltaModule.cs
@@ -60,22 +60,16 @@ namespace PurrNet.Modules
 
         private void ClearTrackers()
         {
-            foreach (var player in _sendingTrackers.Keys)
+            foreach (var clientDict in _sendingTrackers.Values)
             {
-                if (_sendingTrackers.TryGetValue(player, out var clientDict))
-                {
-                    foreach (var tracker in clientDict.Values)
-                        tracker.Dispose();
-                }
+                foreach (var tracker in clientDict.Values)
+                    tracker.Dispose();
             }
 
-            foreach (var player in _receivingTrackers.Keys)
+            foreach (var receiveDict in _receivingTrackers.Values)
             {
-                if (_receivingTrackers.TryGetValue(player, out var receiveDict))
-                {
-                    foreach (var tracker in receiveDict.Values)
-                        tracker.Dispose();
-                }
+                foreach (var tracker in receiveDict.Values)
+                    tracker.Dispose();
             }
 
             _sendingTrackers.Clear();
