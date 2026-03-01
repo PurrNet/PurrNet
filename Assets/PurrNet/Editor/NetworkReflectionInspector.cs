144e01e7018354e4d9d826077d0b2a410ae15ebe
BobsiUnity <martin.skovrup@gmail.com>
2026-03-01 15:08:41 +0100

    Update NetworkReflectionInspector.cs

--- Assets/PurrNet/Editor/NetworkReflectionInspector.cs
+++ Assets/PurrNet/Editor/NetworkReflectionInspector.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEngine;
 
 namespace PurrNet.Editor
@@ -10,7 +14,11 @@ namespace PurrNet.Editor
     {
         private SerializedProperty _trackedBehaviour;
         private SerializedProperty _trackedFields;
+        private SerializedProperty _trackedMethods;
         private SerializedProperty _ownerAuth;
+
+        internal const string CACHE_DIR = "Library/PurrNet";
+        internal const string CACHE_FILE = "Library/PurrNet/ReflectionRPCTargets.txt";
 
         protected override void OnEnable()
         {
@@ -18,12 +26,14 @@ namespace PurrNet.Editor
 
             _trackedBehaviour = serializedObject.FindProperty("_trackedBehaviour");
             _trackedFields = serializedObject.FindProperty("_trackedFields");
+            _trackedMethods = serializedObject.FindProperty("_trackedMethods");
             _ownerAuth = serializedObject.FindProperty("_ownerAuth");
         }
 
-        static readonly List<ReflectionData> _validNames = new List<ReflectionData>();
+        static readonly List<ReflectionData> _validFieldNames = new List<ReflectionData>();
+        static readonly List<ReflectionMethodData> _validMethodNames = new List<ReflectionMethodData>();
 
-        void GetAllValidNames(List<ReflectionData> validNames)
+        void GetAllValidFieldNames(List<ReflectionData> validNames)
         {
             const BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance;
 
@@ -54,11 +64,46 @@ namespace PurrNet.Editor
             }
         }
 
-        int GetIndexOfName(string propName)
+        void GetAllValidMethodNames(List<ReflectionMethodData> validNames)
         {
-            for (var i = 0; i < _validNames.Count; i++)
+            const BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance |
+                                       BindingFlags.DeclaredOnly;
+
+            var reflection = (NetworkReflection)target;
+            var trackedType = reflection.trackedType;
+            var methods = trackedType.GetMethods(flags);
+
+            foreach (var method in methods)
             {
-                if (_validNames[i].name == propName)
+                if (method.IsSpecialName) continue;
+                if (method.IsAbstract) continue;
+                if (method.ReturnType != typeof(void)) continue;
+                if (method.IsGenericMethod) continue;
+
+                validNames.Add(new ReflectionMethodData
+                {
+                    name = method.Name,
+                    rpcType = ReflectionRPCType.ServerRpc
+                });
+            }
+        }
+
+        int GetFieldIndexOfName(string propName)
+        {
+            for (var i = 0; i < _validFieldNames.Count; i++)
+            {
+                if (_validFieldNames[i].name == propName)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        int GetMethodIndexOfName(string methodName)
+        {
+            for (var i = 0; i < _validMethodNames.Count; i++)
+            {
+                if (_validMethodNames[i].name == methodName)
                     return i;
             }
 
@@ -67,7 +112,6 @@ namespace PurrNet.Editor
 
         public override void OnInspectorGUI()
         {
-            // base.OnInspectorGUI();
             DrawDefaultInspector();
 
             var reflection = (NetworkReflection)target;
@@ -83,13 +127,28 @@ namespace PurrNet.Editor
                 return;
             }
 
-            _validNames.Clear();
-            GetAllValidNames(_validNames);
+            _validFieldNames.Clear();
+            GetAllValidFieldNames(_validFieldNames);
 
-            string[] names = new string[_validNames.Count];
+            _validMethodNames.Clear();
+            GetAllValidMethodNames(_validMethodNames);
 
-            for (var i = 0; i < _validNames.Count; i++)
-                names[i] = $"{_validNames[i].type}/{_validNames[i].name}";
+            DrawFieldsSection(reflection);
+
+            EditorGUILayout.Space(8);
+
+            DrawMethodsSection(reflection);
+
+            DrawIdentityInspector();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        void DrawFieldsSection(NetworkReflection reflection)
+        {
+            string[] fieldNames = new string[_validFieldNames.Count];
+            for (var i = 0; i < _validFieldNames.Count; i++)
+                fieldNames[i] = $"{_validFieldNames[i].type}/{_validFieldNames[i].name}";
 
             if (reflection.trackedFields == null)
             {
@@ -97,21 +156,23 @@ namespace PurrNet.Editor
                 EditorUtility.SetDirty(reflection);
             }
 
+            EditorGUILayout.LabelField("Synced Fields", EditorStyles.boldLabel);
             EditorGUI.BeginProperty(Rect.zero, GUIContent.none, _trackedFields);
             EditorGUILayout.BeginVertical("helpbox");
+
             for (var i = 0; i < reflection.trackedFields.Count; i++)
             {
                 EditorGUILayout.BeginHorizontal();
 
-                int idx = GetIndexOfName(reflection.trackedFields[i].name);
+                int idx = GetFieldIndexOfName(reflection.trackedFields[i].name);
 
                 GUI.color = idx == -1 ? Color.yellow : Color.white;
-                int newIdx = EditorGUILayout.Popup(idx, names);
+                int newIdx = EditorGUILayout.Popup(idx, fieldNames);
 
                 if (newIdx != idx)
                 {
                     Undo.RecordObject(reflection, "Change field");
-                    reflection.trackedFields[i] = _validNames[newIdx];
+                    reflection.trackedFields[i] = _validFieldNames[newIdx];
                     EditorUtility.SetDirty(reflection);
                 }
 
@@ -146,10 +207,182 @@ namespace PurrNet.Editor
 
             EditorGUILayout.EndVertical();
             EditorGUI.EndProperty();
+        }
 
-            DrawIdentityInspector();
+        void DrawMethodsSection(NetworkReflection reflection)
+        {
+            var allMethods = reflection.trackedType.GetMethods(
+                BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
 
-            serializedObject.ApplyModifiedProperties();
+            string[] methodDisplayNames = new string[_validMethodNames.Count];
+            for (var i = 0; i < _validMethodNames.Count; i++)
+            {
+                var methodName = _validMethodNames[i].name;
+                MethodInfo methodInfo = null;
+
+                for (int m = 0; m < allMethods.Length; m++)
+                {
+                    if (allMethods[m].Name == methodName)
+                    {
+                        methodInfo = allMethods[m];
+                        break;
+                    }
+                }
+
+                string paramStr = "()";
+                if (methodInfo != null)
+                {
+                    var parameters = methodInfo.GetParameters();
+                    if (parameters.Length > 0)
+                        paramStr = "(" + string.Join(", ", parameters.Select(p => p.ParameterType.Name)) + ")";
+                }
+
+                methodDisplayNames[i] = $"{methodName}{paramStr}";
+            }
+
+            if (reflection.trackedMethods == null)
+            {
+                reflection.trackedMethods = new List<ReflectionMethodData>();
+                EditorUtility.SetDirty(reflection);
+            }
+
+            EditorGUILayout.LabelField("RPC Methods", EditorStyles.boldLabel);
+            EditorGUI.BeginProperty(Rect.zero, GUIContent.none, _trackedMethods);
+            EditorGUILayout.BeginVertical("helpbox");
+
+            bool methodsChanged = false;
+
+            for (var i = 0; i < reflection.trackedMethods.Count; i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+
+                int idx = GetMethodIndexOfName(reflection.trackedMethods[i].name);
+
+                GUI.color = idx == -1 ? Color.yellow : Color.white;
+                int newIdx = EditorGUILayout.Popup(idx, methodDisplayNames);
+
+                if (newIdx != idx)
+                {
+                    Undo.RecordObject(reflection, "Change method");
+                    var data = _validMethodNames[newIdx];
+                    data.rpcType = reflection.trackedMethods[i].rpcType;
+                    reflection.trackedMethods[i] = data;
+                    EditorUtility.SetDirty(reflection);
+                    methodsChanged = true;
+                }
+
+                GUI.color = Color.white;
+
+                var currentRpcType = reflection.trackedMethods[i].rpcType;
+                var newRpcType = (ReflectionRPCType)EditorGUILayout.EnumPopup(currentRpcType, GUILayout.Width(120));
+                if (newRpcType != currentRpcType)
+                {
+                    Undo.RecordObject(reflection, "Change RPC type");
+                    var data = reflection.trackedMethods[i];
+                    data.rpcType = newRpcType;
+                    reflection.trackedMethods[i] = data;
+                    EditorUtility.SetDirty(reflection);
+                }
+
+                GUI.backgroundColor = Color.red;
+                if (GUILayout.Button("Remove", GUILayout.ExpandWidth(false), GUILayout.Width(100)))
+                {
+                    Undo.RecordObject(reflection, "Remove method");
+                    reflection.trackedMethods.RemoveAt(i);
+                    EditorUtility.SetDirty(reflection);
+                    methodsChanged = true;
+                    i--;
+                }
+
+                GUI.backgroundColor = Color.white;
+
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUI.backgroundColor = Color.green;
+            if (GUILayout.Button("Add", GUILayout.ExpandWidth(false), GUILayout.Width(100)))
+            {
+                Undo.RecordObject(reflection, "Add method");
+                reflection.trackedMethods.Add(new ReflectionMethodData());
+                EditorUtility.SetDirty(reflection);
+                methodsChanged = true;
+            }
+
+            GUI.backgroundColor = Color.white;
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+            EditorGUI.EndProperty();
+
+            if (methodsChanged)
+                RefreshReflectionTargetsCache();
+        }
+
+        internal static void RefreshReflectionTargetsCache()
+        {
+            var targetTypes = new HashSet<string>();
+
+            var allReflections = Resources.FindObjectsOfTypeAll<NetworkReflection>();
+            foreach (var reflection in allReflections)
+            {
+                if (reflection.trackedMethods == null || reflection.trackedMethods.Count == 0)
+                    continue;
+
+                var type = reflection.trackedType;
+                if (type != null)
+                    targetTypes.Add(type.AssemblyQualifiedName != null ? type.FullName : type.Name);
+            }
+
+            var prefabGuids = AssetDatabase.FindAssets("t:Prefab");
+            foreach (var guid in prefabGuids)
+            {
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                if (prefab == null) continue;
+
+                var reflections = prefab.GetComponentsInChildren<NetworkReflection>(true);
+                foreach (var reflection in reflections)
+                {
+                    if (reflection.trackedMethods == null || reflection.trackedMethods.Count == 0)
+                        continue;
+
+                    var type = reflection.trackedType;
+                    if (type != null)
+                        targetTypes.Add(type.FullName);
+                }
+            }
+
+            WriteCacheFile(targetTypes);
+        }
+
+        static void WriteCacheFile(HashSet<string> targetTypeNames)
+        {
+            if (!Directory.Exists(CACHE_DIR))
+                Directory.CreateDirectory(CACHE_DIR);
+
+            var sorted = targetTypeNames.OrderBy(n => n).ToArray();
+            var newContent = string.Join("\n", sorted);
+
+            if (File.Exists(CACHE_FILE))
+            {
+                var existing = File.ReadAllText(CACHE_FILE);
+                if (existing == newContent)
+                    return;
+            }
+
+            File.WriteAllText(CACHE_FILE, newContent);
+            CompilationPipeline.RequestScriptCompilation();
+        }
+    }
+
+    static class NetworkReflectionCacheBootstrap
+    {
+        [InitializeOnLoadMethod]
+        static void OnEditorLoad()
+        {
+            EditorApplication.delayCall += NetworkReflectionInspector.RefreshReflectionTargetsCache;
         }
     }
 }
