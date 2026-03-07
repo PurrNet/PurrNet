7ab34addd2d0f6a737875ed12a3b11f5c2b4b961
BobsiUnity <martin.skovrup@gmail.com>
2026-03-07 14:17:26 +0100

    fix: Added Network Addressables helper struct

---
+++ Assets/PurrNet/Runtime/BitPacker/Packers/NetworkAddressables.cs
@@ -0,0 +1,39 @@
+#if ADDRESSABLES_PURRNET_SUPPORT
+using System.Threading.Tasks;
+using PurrNet.Logging;
+using PurrNet.Packing;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace PurrNet
+{
+    public struct NetworkAddressables : IPackedAuto, IAsyncPackable
+    {
+        [DontPack] public AssetReference addressablesReference;
+        private string GUID;
+
+        public Task<IAsyncPackable> PrepareForPackAsync()
+        {
+            if (addressablesReference == null)
+                return Task.FromResult<IAsyncPackable>(this);
+
+            GUID = addressablesReference.AssetGUID;
+            return Task.FromResult<IAsyncPackable>(this);
+        }
+
+        public async Task<IAsyncPackable> PrepareAfterUnpackAsync()
+        {
+            if (string.IsNullOrEmpty(GUID))
+            {
+                PurrLogger.LogError($"Failed to unpack network addressable as the GUID is empty", addressablesReference?.Asset);
+                return this;
+            }
+
+            var handle = Addressables.LoadAssetAsync<Object>(GUID);
+            await handle.Task;
+            addressablesReference = new AssetReference(GUID);
+            return this;
+        }
+    }
+}
+#endif
