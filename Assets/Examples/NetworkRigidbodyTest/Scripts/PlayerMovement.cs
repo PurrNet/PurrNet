6f9c6ad42ebb9c6c5aa5b0562714b9e856c58f3f
BobsiUnity <martin.skovrup@gmail.com>
2026-03-10 00:13:20 +0100

    Network RB testing changes

--- Assets/Examples/NetworkRigidbodyTest/Scripts/PlayerMovement.cs
+++ Assets/Examples/NetworkRigidbodyTest/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using PurrNet;
 using UnityEngine;
 
@@ -8,8 +7,10 @@ namespace NetworkRigidbodyTest
     {
         [SerializeField] private float _moveForce = 5;
         [SerializeField] private float _jumpForce = 5;
+        [SerializeField] private float _acceleration = 50f;
+        [SerializeField] private bool _velocityChangeBased;
         [SerializeField] private NetworkRigidbody _rb;
-    
+
         protected override void OnSpawned()
         {
             base.OnSpawned();
@@ -25,12 +26,26 @@ namespace NetworkRigidbodyTest
         private void FixedUpdate()
         {
             var direction = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
-            _rb.AddForce(direction * (_moveForce * _rb.mass));
+
+            if (_velocityChangeBased)
+            {
+                if (direction.magnitude < 0.1f)
+                    return;
+
+                var currentHoriz = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
+                var velocityDelta = direction * _moveForce - currentHoriz;
+                var maxDelta = _acceleration * Time.fixedDeltaTime;
+                _rb.AddForce(Vector3.ClampMagnitude(velocityDelta, maxDelta), ForceMode.VelocityChange);
+            }
+            else
+            {
+                _rb.AddForce(direction * (_moveForce * _rb.mass));
+            }
         }
 
         private void Update()
         {
-            if(Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space))
                 _rb.AddForce(Vector3.up * (_rb.mass * _jumpForce), ForceMode.Impulse);
         }
     }
