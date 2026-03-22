3a08125ecae1139feb6b8dd98711c28c78d1c6dc
VillerotJustin <62113284+VillerotJustin@users.noreply.github.com>
2026-03-22 17:36:03 +0100

    Revert "Try EvaluateVisibilityForPlayer To debug"
    This reverts commit ca7fdedba0be6afb61204816d3929d7c7abb29f3.

--- Assets/PurrNet/Runtime/CoreModules/HierarchyV2/HierarchyV2.cs
+++ Assets/PurrNet/Runtime/CoreModules/HierarchyV2/HierarchyV2.cs
@@ -427,7 +427,6 @@ namespace PurrNet.Modules
 
             LogCatchupTrace($"TryServerCatchup executing player={playerId} scene={_sceneId}");
             CatchupClient(playerId);
-            EvaluateVisibilityForPlayer(playerId);
         }
 
         private static void LogCatchupTrace(string msg)
