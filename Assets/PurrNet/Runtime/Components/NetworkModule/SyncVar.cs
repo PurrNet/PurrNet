7e14941bbd4019f0090e500373a456e97fdfdf0c
BlenMiner <blenminer123@gmail.com>
2026-04-22 11:19:31 +0200

    fix: cache the TickModule when subscribing inside the SyncVar and use it for unsubscribing for consistency

--- Assets/PurrNet/Runtime/Components/NetworkModule/SyncVar.cs
+++ Assets/PurrNet/Runtime/Components/NetworkModule/SyncVar.cs
@@ -108,13 +108,16 @@ namespace PurrNet
             }
         }
 
+        private TickManager _subscribedTicker;
+
         private void SubscribeToTickManager()
         {
             if (_isSubscribedToTickManager)
                 return;
 
             _isSubscribedToTickManager = true;
-            networkManager.tickModule.onTick += OnTick;
+            _subscribedTicker = networkManager.tickModule;
+            _subscribedTicker.onTick += OnTick;
         }
 
         private void UnsubscribeFromTickManager()
@@ -123,7 +126,7 @@ namespace PurrNet
                 return;
 
             _isSubscribedToTickManager = false;
-            networkManager.tickModule.onTick -= OnTick;
+            _subscribedTicker.onTick -= OnTick;
         }
 
         public override void OnObserverAdded(PlayerID player, bool isSpawner)
