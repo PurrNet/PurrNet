1057c7138a037a9b83d19f7501f4888aa73f5cfd
val <valentin.wce@gmail.com>
2026-04-28 05:44:22 +0000

    NetworkIdentity controller based on owner value.

--- Assets/PurrNet/Runtime/Components/NetworkIdentity/NetworkIdentity.cs
+++ Assets/PurrNet/Runtime/Components/NetworkIdentity/NetworkIdentity.cs
@@ -317,7 +317,7 @@ namespace PurrNet
         /// If you can, cache this value for performance.
         /// </summary>
         public PlayerID? owner => isServer ? internalOwnerServer : internalOwnerClient;
-        public PlayerID? controller => isController ? owner : PlayerID.Server;
+        public PlayerID? controller => owner == null ? PlayerID.Server : owner;
 
         public NetworkManager networkManager { get; private set; }
 
