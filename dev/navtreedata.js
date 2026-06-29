bc0f5954a3538c76e87cbdcdbcb613fb53347ef2
BlenMiner <BlenMiner@users.noreply.github.com>
2026-06-29 12:42:23 +0000

    deploy: f28e50014f642988a796b63afee43ebfeba55876

--- dev/navtreedata.js
+++ dev/navtreedata.js
@@ -54,10 +54,10 @@ var NAVTREEINDEX =
 "classPurrNet_1_1Modules_1_1ScenesModule.html#a56b451ee775be978df48abfaaa6fc8e8",
 "classPurrNet_1_1NetworkIdentity.html#ae554a2054c59f8d4f1e7f9a3480479f4",
 "classPurrNet_1_1NetworkPrefabs.html#a045dabd40864f5ea742adee2aeda9067",
-"classPurrNet_1_1ReflectionRPCTargetAttribute.html",
-"classPurrNet_1_1SyncVar.html#a554329ae401393710c94d605876f48f7",
-"interfacePurrNet_1_1Transports_1_1IListener.html",
-"structPurrNet_1_1Packing_1_1PackedByte.html"
+"classPurrNet_1_1Reference.html#a9bc2bf6a7265664cb9bbc20f2d765916",
+"classPurrNet_1_1SyncVar.html#a36e597796f56c71bb965d6dbe56123d0",
+"interfacePurrNet_1_1Transports_1_1IConnectable.html",
+"structPurrNet_1_1Packing_1_1NormalizedFloat.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
