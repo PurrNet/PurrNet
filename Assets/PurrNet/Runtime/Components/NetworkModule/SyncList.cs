d817bb310fead6b292c41c14264412063bc90375
Vladislav <indraayy322@gmail.com>
2026-07-01 19:48:19 +0500

    fixed some serialized sync types didn't restore initial value on pool reset

--- Assets/PurrNet/Runtime/Components/NetworkModule/SyncList.cs
+++ Assets/PurrNet/Runtime/Components/NetworkModule/SyncList.cs
@@ -78,11 +78,12 @@ namespace PurrNet
     }
 
     [Serializable]
-    public class SyncList<T> : NetworkModule, IList<T>, ITick
+    public class SyncList<T> : NetworkModule, IList<T>, ISerializationCallbackReceiver, ITick
     {
         [SerializeField] private bool _ownerAuth;
         [SerializeField, Min(0)] private float _sendIntervalInSeconds;
         [SerializeField] private List<T> _list = new List<T>();
+        [SerializeField, HideInInspector] private List<T> _initialList = new List<T>();
 
         public List<T> list => _list;
         public List<T> ToList() => _list;
@@ -121,6 +122,7 @@ namespace PurrNet
         {
             onChanged = null;
             _pendingChanges.Clear();
+            RestoreInitialList();
             _lastSendTime = default;
             _wasLastDirty = default;
             _isDirty = default;
@@ -129,12 +131,14 @@ namespace PurrNet
         public SyncList(bool ownerAuth = false)
         {
             _ownerAuth = ownerAuth;
+            CacheInitialList();
         }
 
         public SyncList(List<T> defaultValues, bool ownerAuth = false)
         {
             _list = defaultValues;
             _ownerAuth = ownerAuth;
+            CacheInitialList();
         }
 
         public T this[int idx]
@@ -170,6 +174,18 @@ namespace PurrNet
         {
             _pendingChanges.Add(change);
             _isDirty = true;
+        }
+
+        private void CacheInitialList()
+        {
+            _initialList.Clear();
+            _initialList.AddRange(_list);
+        }
+
+        private void RestoreInitialList()
+        {
+            _list.Clear();
+            _list.AddRange(_initialList);
         }
 
         public override void OnSpawn()
@@ -709,5 +725,14 @@ namespace PurrNet
         }
 
         #endregion
+
+        public void OnBeforeSerialize()
+        {
+        }
+
+        public void OnAfterDeserialize()
+        {
+            CacheInitialList();
+        }
     }
 }
