edfdd58f7be4ad199b394d1c3c538c118797497a
BlenMiner <blenminer123@gmail.com>
2026-07-19 11:01:59 +0200

    fix: compare BitData by value (made delta validator fail)

--- Assets/PurrNet/Runtime/BitPacker/BitData.cs
+++ Assets/PurrNet/Runtime/BitPacker/BitData.cs
@@ -2,7 +2,7 @@
 
 namespace PurrNet.Packing
 {
-    public struct BitData : IDisposable, IDuplicate<BitData>
+    public struct BitData : IDisposable, IDuplicate<BitData>, IEquatable<BitData>
     {
         public readonly BitPacker packer;
         public Size bitOrigin;
@@ -41,6 +41,55 @@ namespace PurrNet.Packing
             var newpacker = BitPackerPool.Get();
             newpacker.Write(this);
             return new BitData(newpacker);
+        }
+
+        public bool Equals(BitData other)
+        {
+            int count = (int)bitLength.value;
+            int otherCount = (int)other.bitLength.value;
+
+            if (packer == null) count = 0;
+            if (other.packer == null) otherCount = 0;
+
+            if (count != otherCount)
+                return false;
+
+            if (count == 0)
+                return true;
+
+            if (ReferenceEquals(packer, other.packer) && bitOrigin.value == other.bitOrigin.value)
+                return true;
+
+            return BitsEqual(packer.buffer, (int)bitOrigin.value, other.packer.buffer, (int)other.bitOrigin.value, count);
+        }
+
+        static bool BitsEqual(byte[] a, int aPos, byte[] b, int bPos, int count)
+        {
+            while (count > 0)
+            {
+                int chunk = count > 56 ? 56 : count;
+                if (PeekBits(a, aPos, chunk) != PeekBits(b, bPos, chunk))
+                    return false;
+                aPos += chunk;
+                bPos += chunk;
+                count -= chunk;
+            }
+
+            return true;
+        }
+
+        static ulong PeekBits(byte[] buffer, int bitPos, int count)
+        {
+            int bytePos = bitPos >> 3;
+            int bitOffset = bitPos & 7;
+            int end = bytePos + 8 < buffer.Length ? bytePos + 8 : buffer.Length;
+
+            ulong raw = 0;
+            for (int i = bytePos; i < end; i++)
+                raw |= (ulong)buffer[i] << ((i - bytePos) * 8);
+
+            raw >>= bitOffset;
+            return raw & ((1UL << count) - 1);
         }
     }
 }
