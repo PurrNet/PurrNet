8b80d51465d63d6c6976757cd462757ff2ac38a3
BobsiUnity <martin.skovrup@gmail.com>
2026-08-04 14:24:06 +0200

    Network prefab lookup test scenario

---
+++ Assets/Tests/BitPacker.Tests/NetworkPrefabsLookupTests.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using PurrNet;
+using UnityEngine;
+
+public class NetworkPrefabsLookupTests
+{
+    const string SHARED_NAME = "SharedPrefabName";
+
+    readonly List<Object> _created = new();
+
+    [TearDown]
+    public void TearDown()
+    {
+        for (int i = 0; i < _created.Count; i++)
+        {
+            if (_created[i])
+                Object.DestroyImmediate(_created[i]);
+        }
+
+        _created.Clear();
+    }
+
+    NetworkPrefabs CreateProvider()
+    {
+        var provider = ScriptableObject.CreateInstance<NetworkPrefabs>();
+        provider.autoGenerate = false;
+        _created.Add(provider);
+        return provider;
+    }
+
+    GameObject CreateNetworkedPrefab(string name)
+    {
+        var go = new GameObject(name);
+        go.AddComponent<NetworkIdentity>();
+        _created.Add(go);
+        return go;
+    }
+
+    GameObject CreatePlainPrefab(string name)
+    {
+        var go = new GameObject(name);
+        _created.Add(go);
+        return go;
+    }
+
+    [Test]
+    public void ReferenceMatchResolvesRegisteredPrefab()
+    {
+        var provider = CreateProvider();
+        var networked = CreateNetworkedPrefab(SHARED_NAME);
+        provider.AddRuntimePrefab("registered", networked);
+
+        Assert.That(provider.TryGetPrefabData(networked, out var data), Is.True);
+        Assert.That(data.prefab, Is.EqualTo(networked));
+    }
+
+    [Test]
+    public void NonNetworkedPrefabSharingNameIsNotResolved()
+    {
+        var provider = CreateProvider();
+        var networked = CreateNetworkedPrefab(SHARED_NAME);
+        provider.AddRuntimePrefab("registered", networked);
+
+        var foreign = CreatePlainPrefab(SHARED_NAME);
+
+        Assert.That(provider.TryGetPrefabData(foreign, out var data), Is.False,
+            $"An unregistered prefab with no NetworkIdentity resolved to `{data.prefab}` purely because of a name collision.");
+    }
+
+    [Test]
+    public void NetworkedPrefabCopySharingNameStillResolves()
+    {
+        var provider = CreateProvider();
+        var networked = CreateNetworkedPrefab(SHARED_NAME);
+        provider.AddRuntimePrefab("registered", networked);
+
+        var bundleCopy = CreateNetworkedPrefab(SHARED_NAME);
+
+        Assert.That(provider.TryGetPrefabData(bundleCopy, out var data), Is.True);
+        Assert.That(data.prefab, Is.EqualTo(networked));
+    }
+
+    [Test]
+    public void NetworkedPrefabWithDifferentIdentityCountIsNotResolved()
+    {
+        var provider = CreateProvider();
+        var networked = CreateNetworkedPrefab(SHARED_NAME);
+        provider.AddRuntimePrefab("registered", networked);
+
+        var unrelated = CreateNetworkedPrefab(SHARED_NAME);
+        var child = new GameObject("Nested");
+        child.AddComponent<NetworkIdentity>();
+        child.transform.SetParent(unrelated.transform);
+
+        Assert.That(provider.TryGetPrefabData(unrelated, out _), Is.False);
+    }
+
+    [Test]
+    public void AmbiguousNameIsNotResolved()
+    {
+        var provider = CreateProvider();
+        provider.AddRuntimePrefab("registered-a", CreateNetworkedPrefab(SHARED_NAME));
+        provider.AddRuntimePrefab("registered-b", CreateNetworkedPrefab(SHARED_NAME));
+
+        var bundleCopy = CreateNetworkedPrefab(SHARED_NAME);
+
+        Assert.That(provider.TryGetPrefabData(bundleCopy, out _), Is.False);
+    }
+}
